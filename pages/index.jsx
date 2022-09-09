e7036fff5718049f89af244aab6eff64eb0993e4
MatricksDeCoder <infinitumcubed@gmail.com>
2022-09-09 19:16:17 +0200

    changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -55,10 +55,10 @@ const Home = () => {
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
         <div>
-          <p>Search Address on Ethereum for NFT Collection e.g vitalik.eth or </p>
+          <p>Search Address on Ethereum for NFT Collection e.g vitalik.eth or 0xd8da6bf26964af9d7eed9e03e53415d37aa96045</p>
         </div>
-        <input disabled={fetchForCollection} onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
-        <input onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="Add the collection address"></input>
+        <input className="border border-sky-500" disabled={fetchForCollection} onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
+        <input className="border border-sky-500" onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="Add the collection address"></input>
         <label className="text-gray-600 "><input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
         <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
            () => {
